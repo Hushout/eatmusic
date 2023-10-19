518cd5f4284a7e9bb84c3b643038c05946223b8d
Lam <caoanhlamvn@gmail.com>
2023-10-19 15:31:32 +0200

    onScroll hide navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,16 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import './NavBar.scss';
 import { Link } from "react-router-dom";
 
-interface navBarProps {
+interface NavBarProps {
     paths: { path: string; name: string; icon?: FC }[]
 }
 
-const NavBar: FC<navBarProps> = ({ paths }: navBarProps) => {
+const NavBar: FC<NavBarProps> = ({ paths }: NavBarProps) => {
+    const [hideNavBar, setHideNavBar] = useState<boolean>(false);
+    const [prevScrollPos, setPrevScrollPos] = useState<number>(window.scrollY);
 
-    return <div className="NavBar">
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            if (currentScrollPos > prevScrollPos) {
+                setHideNavBar(true);
+            } else {
+                setHideNavBar(false);
+            }
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [prevScrollPos]);
+
+    return <div className={`NavBar ${hideNavBar ? "hide" : ""}`}>
         {paths.map(path => {
-            return <Link key={path.name} to={path.path}>{path.icon ? <path.icon /> : path.name}</Link>
+            return <Link key={path.name} className={path.name + "Link"} to={path.path}>{path.icon ? <path.icon /> : path.name}</Link>
         })}
     </div>;
 }
