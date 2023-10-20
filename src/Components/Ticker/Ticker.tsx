1067a50c5e9bb4264f11e131e48090a70ca3cfd8
Lam <caoanhlamvn@gmail.com>
2023-10-20 14:46:03 +0200

    ticker added

---
+++ src/Components/Ticker/Ticker.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from "react";
+import './Ticker.scss'
+
+const Ticker: FC = () => {
+    return (
+        <div className="Ticker">
+            <div className="tickerTape">
+                <span>ReactJs</span>
+                <span>Redux</span>
+                <span>NodeJs</span>
+                <span>AWS</span>
+                <span>SCSS</span>
+                <span>ThreeJs</span>
+                <span>Web-Audio-API</span>
+                <span>MongoDB</span>
+                <span>Docker</span>
+                <span>GraphQL</span>
+                <span>Typescript</span>
+                <span>By CAO ANH</span>
+            </div>
+            <div className="tickerTape">
+                <span>ReactJs</span>
+                <span>Redux</span>
+                <span>NodeJs</span>
+                <span>AWS</span>
+                <span>SCSS</span>
+                <span>ThreeJs</span>
+                <span>Web-Audio-API</span>
+                <span>MongoDB</span>
+                <span>Docker</span>
+                <span>GraphQL</span>
+                <span>Typescript</span>
+                <span>By CAO ANH</span>
+            </div>
+        </div>
+    );
+}
+
+export default Ticker;
