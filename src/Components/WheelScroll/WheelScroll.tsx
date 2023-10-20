9d84b99ad3eb4788588a364c42b82675e1facfe4
Lam <caoanhlamvn@gmail.com>
2023-10-20 13:09:24 +0200

    WheelScroll animated on scroll

---
+++ src/Components/WheelScroll/WheelScroll.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useEffect, useState } from "react";
+import './WheelScroll.scss'
+
+const WheelScroll: FC = () => {
+
+    const [rotationAngle, setRotationAngle] = useState(0);
+
+    const handleWheel = (event: WheelEvent) => {
+      const delta = event.deltaY;
+      const sensitivity:number = 1;
+      setRotationAngle(rotationAngle + delta * sensitivity);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("wheel", handleWheel);
+      return () => {
+        window.removeEventListener("wheel", handleWheel);
+      };
+    }, [rotationAngle]);
+  
+    const style = {
+      transform: `rotate(${rotationAngle}deg)`,
+    };
+
+    return <div className="WheelScroll" style={style}>
+        <img src="/vinyl.png" alt="Vinyl"/>
+    </div>;
+}
+
+export default WheelScroll;
