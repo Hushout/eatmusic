ba190a9bb28f92ef610b783e6b6b5d5a93f21b5a
Hushout <caoanhlamvn@gmail.com>
2024-04-16 19:11:02 +0200

    last touch

--- src/Components/Background/Background.tsx
+++ src/Components/Background/Background.tsx
@@ -11,10 +11,17 @@ const Background = () => {
             mouseControls: true,
             touchControls: true,
             gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            separation: 80.00,
+            speedLimit: 4.00,
+            scale: 1.00,
+            scaleMobile: 1.00,
+            backgroundColor: 0x36367a
         });
 
         return () => {
-            if (vantaEffect) vantaEffect.destroy(); 
+            if (vantaEffect) vantaEffect.destroy();
         };
     }, []);
 
