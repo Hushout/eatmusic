cb2120092bd4655d5b4adcb46c94527aaaedd70b
Lam <caoanhlamvn@gmail.com>
2024-04-16 13:16:44 +0200

    last update

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from "react";
+import { useSpring, animated } from "react-spring";
 import "./Home.scss";
 
 const Home: FC = () => {
-  return <div className="Home"></div>;
+  return <div className="Home">
+      <div className="Profile">
+      </div>
+  </div>;
 }
 
 export default Home;
