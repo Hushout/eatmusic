6389b931e8c70cea32e76d28a415c09cad345fa0
Hushout <caoanhlamvn@gmail.com>
2024-04-16 15:52:38 +0200

    adding more style custom parametter to button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,20 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./Button.scss";
 
 interface ButtonProps {
     children: string;
     onClick?: () => void;
+    boxStyle?: {};
+    fontStyle?: string;
 }
 
-const Button: FC<ButtonProps> = ({ children, onClick = () => null }: ButtonProps) => {
+const Button: FC<ButtonProps> = ({ children, onClick = () => null, boxStyle = {}, fontStyle = "white" }: ButtonProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const spanStyle = {
+        color: isHovered ? fontStyle : 'transparent'
+    };
+    const contentButtonStyle = {
+        color: isHovered ? 'transparent' : fontStyle
+    };
     return (
-        <div className="Button" onClick={onClick}>
+        <div className="Button" onClick={onClick} style={boxStyle} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <div className="contentButton" style={contentButtonStyle}>
                 {children}
+            </div>
             <span>
-                <span>{children}</span>
-                <span>{children}</span>
-            </span>
-        </div>
+                <span style={spanStyle}>{children}</span>
+                <span style={spanStyle}>{children}</span>
+            </span >
+        </div >
     );
 }
 
