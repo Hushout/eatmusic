c793514c5b4fb97a70a3aefe33969fec55fac054
Hushout <caoanhlamvn@gmail.com>
2024-04-17 19:29:12 +0200

    finishing skill pages

--- src/Pages/Skill/Skill.tsx
+++ src/Pages/Skill/Skill.tsx
@@ -1,7 +1,44 @@
 import React, { FC } from "react";
+import "./Skill.scss";
+import Card from "../../Components/Card/Card";
+import { title } from "process";
 
 const Skill: FC = () => {
-    return <div>Skill</div>;
+    const skills: { title: string, content: string, img: string }[] = [
+        {
+            title: "Leadership",
+            content: "I have been a team leader in a few projects and I have experience in organizing and managing a team.",
+            img: "https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            title: "Communication",
+            content: "I have good communication skills and I am able to work in a team.",
+            img: "https://images.pexels.com/photos/3184431/pexels-photo-3184431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            title: "Professionalism",
+            content: "I am professional and I am able to work under pressure.",
+            img: "https://images.pexels.com/photos/5673488/pexels-photo-5673488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        {
+            title: "Creativity",
+            content: "I am creative and I like to think outside the box.",
+            img: "https://images.pexels.com/photos/2832382/pexels-photo-2832382.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+    ];
+
+    const skillCards = skills.map(skill => {
+        return (
+            <Card content={skill}/>
+        )
+    });
+
+    return <div className="SkillPage">
+        <div className="title">Skill</div>
+        <div className="skillCard">
+            {skillCards}
+        </div>
+    </div>;
 }
 
 export default Skill;
