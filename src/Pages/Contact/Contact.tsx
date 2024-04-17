6caff39c28ce2730f55a9c4c836c884cd676f15a
Hushout <caoanhlamvn@gmail.com>
2024-04-17 16:30:46 +0200

    contact page created

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,8 +1,21 @@
 import React, { FC } from "react";
 import './Contact.scss';
+import Linka from "../../Components/Linka/Linka";
 
 const Contact: FC = () => {
-    return <div>Contact</div>;
+    return <div className="ContactPage">
+        <div className="title">Contact</div>
+        <div className="contentContact">
+            <div className="leftBox">
+                <div className="subTitle">E-MAIL</div>
+                <Linka href="mailto:Caoanhlamvn@gmail.com" target="_blank">↗ Caoanhlamvn@gmail.com</Linka>
+            </div>
+            <div className="rightBox">
+                <div className="subTitle">E-MAIL</div>
+                <Linka href="mailto:Caoanhlamvn@gmail.com" target="_blank">↗ Caoanhlamvn@gmail.com</Linka>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Contact;
