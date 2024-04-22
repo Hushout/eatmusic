904bf6b964364f31052fb04dbb44dd8e68f0ea0e
Hushout <caoanhlamvn@gmail.com>
2024-04-22 16:27:49 +0200

    modify Card to fit project showcase

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -4,14 +4,15 @@ import './Card.scss';
 interface CardProps {
     content: {
         title: string;
-        content: ReactNode;
+        content?: ReactNode;
         img: string;
-    }
+    },
+    style?: object;
 }
 
-const Card: FC<CardProps> = ({ content }) => {
+const Card: FC<CardProps> = ({ content, style = {} }) => {
     return (
-        <div className="Card">
+        <div className="Card" style={style}>
             <img src={content.img} alt={content.title} />
             <div className="titleCard">{content.title}</div>
             <div className="contentCard">{content.content}</div>
